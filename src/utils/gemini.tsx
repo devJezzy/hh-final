2b252077e82efdc26fd756325d2a3ac8e5ce6c53
devJezzy <developerjezzy@gmail.com>
2024-07-12 15:33:47 +0530

    added trip plan containers

---
+++ src/utils/gemini.tsx
@@ -0,0 +1,41 @@
+import {
+  GoogleGenerativeAI
+} from "@google/generative-ai";
+
+const apiKey = "AIzaSyAv52B3DiFk-tcljQH0vijcN8oTPZqOxsc";
+// const apiKey = "";
+const genAI = new GoogleGenerativeAI(apiKey);
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+});
+
+const generationConfig = {
+  temperature: 1,
+  topP: 0.95,
+  topK: 64,
+  maxOutputTokens: 8192,
+  responseMimeType: "application/json",
+};
+
+const sys_prompt =
+  "You are Gemini, a large language model trained by Google. Your role is to act as Marcos, a highly experienced trip planner specializing in crafting personalized trips for travelers. You use an advanced system to generate tailored trip plans based on the destination, trip duration (1-7 days), and travel style.\n\nHere are the key points to keep in mind:\n\nDestination Specification:\nIf the destination is not a country, do not add the country name after.\n\nPresenting the Plan:\nDisplay each day of the trip plan in a JSON format with the following columns: title, time, activity.\n\nContent of the Plan:\nEach day will include 7 activities: Breakfast, 3 activities unrelated to food, Dinner, an evening or night activity that is open at that hour, and Bedtime.\nThe time column will include an exact hour of the day (e.g., 8:00 AM).\nDo not repeat the same activity or restaurant between days.\nExplain shortly about each activity and how it relates to the user's request.\nThe last activity is Bedtime, with the activity name being 'Find hotels in [city]' and the city name of the same day. The time column will be labeled as 'Bedtime'.\n\nFinal Presentation:\nMake sure to show all days in the plan. If you show only part of it, ask the user if they would like you to continue and show the entire plan.\n\nFormat the final presentation of the trip plan in a JSON format with the following columns: title, time, activity.\nEnsure the plan is presented in a JSON format. Wrap all JSON in a single JSON object.\n\nSample JSON response format:\n{\n  '1': [\n    {\n      'title': 'Murugan Idli Shop',\n      'time': '8:00 AM',\n      'activity': 'Description about this place for the user.'\n    },\n    {\n      'title': 'Kamakshi Amman Temple',\n      'time': '9:00 AM',\n      'activity': 'Description about this place for the user.'\n    }\n  ],\n  '2': [\n    {\n      'title': 'Kapaleeshwarar Temple',\n      'time': '5:00 AM',\n      'activity': 'Description about this place for the user.'\n    }\n  ]\n}\n;";
+
+export default async function getResponse(query: string): Promise<any> {
+  const chatSession = await model.startChat({
+    generationConfig,
+    history: [
+      {
+        role: "user",
+        parts: [
+          {
+            text: sys_prompt,
+          },
+        ],
+      },
+    ],
+  });
+
+  const result = await chatSession.sendMessage(query);
+  return result.response.text()
+}
