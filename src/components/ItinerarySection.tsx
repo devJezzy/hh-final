651b112e4f015d5533a4dcb1130ee777c3b72540
devJezzy <developerjezzy@gmail.com>
2024-07-12 15:56:16 +0530

    added Blob loading animation

--- src/components/ItinerarySection.tsx
+++ src/components/ItinerarySection.tsx
@@ -3,6 +3,7 @@ import DaySelector from "./DaySelector";
 import ItineraryItem from "./ItineraryItem";
 import getResponse from "@/utils/gemini";
 import searchImages from "@/utils/getImage";
+import Blob from "./Blob";
 
 const ItinerarySection: React.FC = () => {
   const [selectedDay, setSelectedDay] = useState(1);
@@ -34,29 +35,33 @@ const ItinerarySection: React.FC = () => {
   const days = Object.keys(itinerary);
 
   return (
-    <div className="flex flex-col w-1/2 max-md:ml-0 max-md:w-full">
-      <div className="flex flex-col p-6 text-base leading-7 text-black rounded-xl border border-solid border-[#9AAC47] border-opacity-30 max-md:px-5 max-md:mt-5 max-md:max-w-full overflow-hidden mb-5 h-full">
-        <div className="flex gap-2 self-start text-sm tracking-tight text-[#9AAC47]">
-          {days.map((day, index) => (
-            <button
-              key={index}
-              className={`justify-center px-7 py-2 rounded-3xl border border-[#9AAC47] border-solid max-md:px-5 ${
-                selectedDay === parseInt(day) ? "text-white bg-[#9AAC47]" : ""
-              }`}
-              onClick={() => setSelectedDay(parseInt(day))}
-            >
-              Day {day}
-            </button>
-          ))}
-        </div>
-        <div className="plans-container overflow-hidden-vertical">
-        {itinerary[selectedDay.toString()] &&
-          itinerary[selectedDay.toString()].map((item, index) => (
-            <ItineraryItem key={index} {...item} />
-          ))}
-          </div>
-      </div>
+<div className="flex flex-col w-1/2 max-md:ml-0 max-md:w-full">
+  <div className="flex flex-col p-6 text-base leading-7 text-black rounded-xl border border-solid border-[#9AAC47] border-opacity-30 max-md:px-5 max-md:mt-5 max-md:max-w-full overflow-hidden mb-5 h-full">
+    <div className="flex gap-2 self-start text-sm tracking-tight text-[#9AAC47]">
+      {days.length === 0 ? (
+        <Blob />
+      ) : (
+        days.map((day, index) => (
+          <button
+            key={index}
+            className={`justify-center px-7 py-2 rounded-3xl border border-[#9AAC47] border-solid max-md:px-5 ${
+              selectedDay === parseInt(day) ? "text-white bg-[#9AAC47]" : ""
+            }`}
+            onClick={() => setSelectedDay(parseInt(day))}
+          >
+            Day {day}
+          </button>
+        ))
+      )}
     </div>
+    <div className="plans-container overflow-hidden-vertical">
+      {itinerary[selectedDay.toString()] &&
+        itinerary[selectedDay.toString()].map((item, index) => (
+          <ItineraryItem key={index} {...item} />
+        ))}
+    </div>
+  </div>
+</div>
   );
 };
 
